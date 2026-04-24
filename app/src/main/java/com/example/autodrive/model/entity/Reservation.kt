5ab0214cd056c7f4316cedfb4596efddf7fdb0d9
LuckyyKid <bafingsimbokeita@gmail.com>
2026-04-23 21:20:30 -0400

    Ajout entité

--- app/src/main/java/com/example/autodrive/model/entity/Reservation.kt
+++ app/src/main/java/com/example/autodrive/model/entity/Reservation.kt
@@ -1,4 +1,32 @@
 package com.example.autodrive.model.entity
 
-class Reservation {
-}
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "reservation",
+    foreignKeys = [
+        ForeignKey(
+            entity = Voiture::class,
+            parentColumns = ["id"],
+            childColumns = ["voitureId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Utilisateur::class,
+            parentColumns = ["id"],
+            childColumns = ["utilisateurId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class Reservation(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val utilisateurId: Long,
+    val voitureId: Long,
+    val dateDebut: String,
+    val dateFin: String,
+    val coutTotal: Double,
+    val statut: String
+)
